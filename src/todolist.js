13904e65ef72c34341c9fc0c63b36ddbccf3d8c7
Vlad <vlad@hotmail.co.nz>
2018-01-31 15:36:04 +1300

    todos modal

--- src/todolist.js
+++ src/todolist.js
@@ -1,12 +1,64 @@
 const createProjectButton = document.querySelector('.button.create.is-primary');
+
+
 const createProjectModal = document.querySelector('.modal');
-
-
-function createProject() {
+function createProjectDisplay() {
   createProjectModal.classList.add('is-active');
   const modalCard = document.querySelector('.modal-card');
   setTimeout(() => modalCard.style.transform = 'translateY(0px)');
 }
 
-createProjectButton.addEventListener('click', createProject);
+createProjectButton.addEventListener('click', createProjectDisplay);
 
+
+const projectInput = document.querySelector('.input.project.is-primary');
+const modalCard = document.querySelector('.modal-card');
+function createProject(e) { 
+  if (e.target.textContent === 'Create') {
+    if (projectInput.value !== "") {
+      projectArray.push(createProjectFactory(projectInput.value));
+      createProjectModal.classList.remove('is-active');
+      modalCard.style.transform = 'translateY(-500px)'
+    } else {
+      alert('Enter a project name or cancel');
+    }
+  
+  } else if (e.target.textContent === 'Cancel') {
+    createProjectModal.classList.remove('is-active');
+    modalCard.style.transform = 'translateY(-500px)'
+  } else if (e.target.className === 'delete') {
+    createProjectModal.classList.remove('is-active');
+    modalCard.style.transform = 'translateY(-500px)'
+  }
+  
+}
+
+modalCard.addEventListener('click', createProject);
+
+const projectMenuItem = document.querySelector('.menu-items');
+function createProjectFactory(projectName) {
+  //append project
+  const projectButton = document.createElement('a');
+  projectButton.setAttribute('class','button project');
+  projectButton.textContent = projectName;
+  projectMenuItem.appendChild(projectButton);
+
+
+  return { projectName }
+}
+
+let projectArray = [];
+projectArray.push(createProjectFactory('Astravisual'));
+
+const projectMenu = document.querySelector('.menu');
+const modalTask = document.querySelector('.modal.task');
+const modalCardTask = document.querySelector('.modal-card.task');
+console.log(modalTask);
+
+function openProjectModal(e) {
+  if (e.target.className === 'button project') {
+    modalTask.classList.add('is-active');
+    setTimeout(() => modalCardTask.style.transform = 'translateY(0px)');
+  }
+}
+projectMenu.addEventListener('click',openProjectModal);
