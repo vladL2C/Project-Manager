c16909c8a704993841abff9389867a247f840336
Vlad <Vlad@hotmail.co.nz>
2018-02-03 14:53:07 +1300

    organized

--- src/todolist.js
+++ src/todolist.js
@@ -1,94 +1,84 @@
 let projectArray = [];
 
-const createProjectButton = document.querySelector('.button.create.is-primary');
+const createProjectButton = document.querySelector(".button.create.is-primary");
 
-
-const createProjectModal = document.querySelector('.modal');
+const createProjectModal = document.querySelector(".modal");
 function createProjectDisplay() {
-  createProjectModal.classList.add('is-active');
-  const modalCard = document.querySelector('.modal-card');
-  setTimeout(() => modalCard.style.transform = 'translateY(0px)');
+  createProjectModal.classList.add("is-active");
+  const modalCard = document.querySelector(".modal-card");
+  setTimeout(() => (modalCard.style.transform = "translateY(0px)"));
 }
 
-createProjectButton.addEventListener('click', createProjectDisplay);
+createProjectButton.addEventListener("click", createProjectDisplay);
 
-
-const projectInput = document.querySelector('.input.project.is-primary');
-const modalCard = document.querySelector('.modal-card');
-function createProject(e) { 
-  if (e.target.textContent === 'Create') {
+const projectInput = document.querySelector(".input.project.is-primary");
+const modalCard = document.querySelector(".modal-card");
+function createProject(e) {
+  if (e.target.textContent === "Create") {
     if (projectInput.value !== "") {
       projectArray.push(createProjectFactory(projectInput.value));
       renderProjects(projectArray);
-      createProjectModal.classList.remove('is-active');
-      modalCard.style.transform = 'translateY(-500px)'
+      createProjectModal.classList.remove("is-active");
+      modalCard.style.transform = "translateY(-500px)";
     } else {
-      alert('Enter a project name or cancel');
+      alert("Enter a project name or cancel");
     }
-  
-  } else if (e.target.textContent === 'Cancel') {
-    createProjectModal.classList.remove('is-active');
-    modalCard.style.transform = 'translateY(-500px)'
-  } else if (e.target.className === 'delete') {
-    createProjectModal.classList.remove('is-active');
-    modalCard.style.transform = 'translateY(-500px)'
+  } else if (e.target.textContent === "Cancel") {
+    createProjectModal.classList.remove("is-active");
+    modalCard.style.transform = "translateY(-500px)";
+  } else if (e.target.className === "delete") {
+    createProjectModal.classList.remove("is-active");
+    modalCard.style.transform = "translateY(-500px)";
   }
-  
 }
 
-modalCard.addEventListener('click', createProject);
+modalCard.addEventListener("click", createProject);
 
-const projectMenuItem = document.querySelector('.menu-items');
+const projectMenuItem = document.querySelector(".menu-items");
 
-const todoInbox = document.querySelector('.inbox');
+const todoInbox = document.querySelector(".inbox");
 
-  function removeTodos(e) {
-    if (e.target.type === 'checkbox') return;
-    if (e.target.parentNode.id === 'del') {
-      project.removeTodo(e.target.parentNode.dataset.idx);
-    }
-    project.renderTodos();
-    
-  } 
-  todoInbox.addEventListener('click', removeTodos);
-
-  function updateChecked(e) {
-    project.updateChecked(parseInt(e.target.dataset.idx));
+function removeTodos(e) {
+  if (e.target.type === "checkbox") return;
+  if (e.target.parentNode.id === "del") {
+    project.removeTodo(e.target.parentNode.dataset.idx);
   }
+  project.renderTodos();
+}
+todoInbox.addEventListener("click", removeTodos);
 
+function updateChecked(e) {
+  project.updateChecked(parseInt(e.target.dataset.idx));
+}
 
-  todoInbox.addEventListener('change', updateChecked);
-
-
+todoInbox.addEventListener("change", updateChecked);
 
 function createProjectFactory(projectName, todos = []) {
-  
   function addTodo(todoText) {
     this.todos.push({ todoText, checked: false });
   }
 
-
   function createTodos(todos) {
     todoInbox.innerHTML = "";
-    todos.forEach((todo,index) => {
-      const itemContainer = document.createElement('div');
-      itemContainer.setAttribute('class','item');
+    todos.forEach((todo, index) => {
+      const itemContainer = document.createElement("div");
+      itemContainer.setAttribute("class", "item");
       todoInbox.appendChild(itemContainer);
 
-      const checkbox = document.createElement('input');
+      const checkbox = document.createElement("input");
       checkbox.type = "checkbox";
       checkbox.dataset.idx = index;
       checkbox.checked = todo.checked;
       itemContainer.appendChild(checkbox);
 
-      let pContent = document.createElement('p');
+      let pContent = document.createElement("p");
       itemContainer.appendChild(pContent);
       pContent.textContent = todo.todoText;
 
-      const deleteButton = document.createElement('i');
+      const deleteButton = document.createElement("i");
       deleteButton.dataset.idx = index;
-      deleteButton.id = 'del';
-      deleteButton.setAttribute('class','fas fa-trash');
+      deleteButton.id = "del";
+      deleteButton.setAttribute("class", "fas fa-trash");
       itemContainer.appendChild(deleteButton);
     });
   }
@@ -98,24 +88,32 @@ function createProjectFactory(projectName, todos = []) {
   }
 
   function removeTodo(position) {
-    this.todos.splice(position,1);
+    this.todos.splice(position, 1);
   }
 
   function updateChecked(target) {
-   return this.todos[target].checked = !this.todos[target].checked;
+    return (this.todos[target].checked = !this.todos[target].checked);
   }
 
-  return { projectName, todos, addTodo, createTodos, renderTodos, removeTodo, updateChecked };
+  return {
+    projectName,
+    todos,
+    addTodo,
+    createTodos,
+    renderTodos,
+    removeTodo,
+    updateChecked
+  };
 }
 
 function renderProjects(projects) {
   projectMenuItem.innerHTML = "";
-  projects.forEach((project,index) => {
-  const projectButton = document.createElement('a');
-  projectButton.setAttribute('class','button project');
-  projectButton.textContent = project.projectName;
-  projectButton.dataset.projectIndex = index;
-  projectMenuItem.appendChild(projectButton);
+  projects.forEach((project, index) => {
+    const projectButton = document.createElement("a");
+    projectButton.setAttribute("class", "button project");
+    projectButton.textContent = project.projectName;
+    projectButton.dataset.projectIndex = index;
+    projectMenuItem.appendChild(projectButton);
   });
 }
 
@@ -124,72 +122,69 @@ let project;
 
 //project modal to add todo's
 
-const modalTask = document.querySelector('.modal.task');
-const modalCardTask = document.querySelector('.modal-card.task');
-
+const modalTask = document.querySelector(".modal.task");
+const modalCardTask = document.querySelector(".modal-card.task");
 
 function openProjectModal(e) {
-  if (e.target.className === 'button project') {
-    modalTask.classList.add('is-active');
+  if (e.target.className === "button project") {
+    modalTask.classList.add("is-active");
     currentOpenProject = parseInt(e.target.dataset.projectIndex);
     project = projectArray[currentOpenProject];
-    setTimeout(() => modalCardTask.style.transform = 'translateY(0px)');
+    setTimeout(() => (modalCardTask.style.transform = "translateY(0px)"));
   }
   project.renderTodos();
 }
-const projectMenu = document.querySelector('.menu');
-projectMenu.addEventListener('click',openProjectModal);
+const projectMenu = document.querySelector(".menu");
+projectMenu.addEventListener("click", openProjectModal);
 
 //add todo's
-const todoInput = document.querySelector('.input.todo.is-primary');
+const todoInput = document.querySelector(".input.todo.is-primary");
 function addTodo() {
   project.addTodo(todoInput.value);
   todoInput.value = "";
   project.renderTodos();
-
 }
 
 //cancel todo modal
-const cancelTodo = document.querySelector('.delete.cancel');
-cancelTodo.addEventListener('click', function() {
-  modalTask.classList.remove('is-active');
-  modalCardTask.style.transform = 'translateY(-800px)';
-
+const cancelTodo = document.querySelector(".delete.cancel");
+cancelTodo.addEventListener("click", function() {
+  modalTask.classList.remove("is-active");
+  modalCardTask.style.transform = "translateY(-800px)";
 });
 
-const addTodoButton = document.querySelector('.button.add.is-success');
-addTodoButton.addEventListener('click',addTodo);
+const addTodoButton = document.querySelector(".button.add.is-success");
+addTodoButton.addEventListener("click", addTodo);
 
 //localStorage save
-const saveButton = document.querySelector('.button.save');
-saveButton.addEventListener('click', saveProjects);
+const saveButton = document.querySelector(".button.save");
+saveButton.addEventListener("click", saveProjects);
 function saveProjects() {
   localStorage.clear();
-  localStorage.setItem('projects', JSON.stringify(projectArray));
-  alert('projects saved');
+  localStorage.setItem("projects", JSON.stringify(projectArray));
+  alert("projects saved");
 }
 
 let loaded = false;
 //localStorage load
-const loadButton = document.querySelector('.button.load');
+const loadButton = document.querySelector(".button.load");
 function loadProjects() {
   if (loaded !== false) {
-    alert('already loaded');
-    return
+    alert("already loaded");
+    return;
   }
-  const mySavedProjects = JSON.parse(localStorage.getItem('projects'));
-  mySavedProjects.forEach((project) => {
-    projectArray.push(createProjectFactory(project.projectName, [...project.todos]));
+  const mySavedProjects = JSON.parse(localStorage.getItem("projects"));
+  mySavedProjects.forEach(project => {
+    projectArray.push(
+      createProjectFactory(project.projectName, [...project.todos])
+    );
   });
   loaded = true;
   renderProjects(projectArray);
 }
 
-loadButton.addEventListener('click', loadProjects);
+loadButton.addEventListener("click", loadProjects);
 //delete project
 
 //pre render a project
 //projectArray.push(createProjectFactory('Astravisual'));
 //renderProjects(projectArray);
-
-
